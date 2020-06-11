1cd705bb6fae44bafe1c2d5462c096139ccf084d
Hudrolax <hudro795@gmail.com>
2020-06-11 11:02:34 +0300

    bugfix

--- test/config.py
+++ test/config.py
@@ -2,7 +2,7 @@ JARVIS_PATH = '/home/pi/jarvis/'
 ARDUINO_CONFIG_NAME = 'config.txt'
 ARDUINO_PINSTATE_FILENAME = 'arduino_pinstate.txt'
 GOOD_PROXY_LIST = 'good_proxylist.txt'
-API_TOKEN = '1123277123:AAFz7b_joMY-4yGavFAE5o5MKstU5cz5Cfw'
+API_TOKEN = ''
 NOT_IMPORTANT_WORDS = ['в', 'на', 'к', 'у', 'для', 'за']
 
 MINER_PATH = 'c:/eth/'
